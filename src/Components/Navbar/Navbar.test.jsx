3f3e9b382b6dc2978bba06e27341e6ae9fc8fcd2
Ricardo-A-Zapata <raz6675@nyu.edu>
2025-02-25 23:14:04 -0500

    Created Testing for Navbar.jsx
    Created Navbar.test.jsx to improve testing robustness for frontend and specifically Navbar.jsx.
    
    Summary:
       - Tested Navbar.jsx with Navbar.test.jsx

---
+++ src/Components/Navbar/Navbar.test.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './Navbar';
+
+describe('Navbar Component', () => {
+  test('renders without crashing', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+  });
+
+  test('renders correct number of navigation links', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+
+    const links = screen.getAllByRole('link');
+    expect(links.length).toBe(3); // Since we have 3 pages in PAGES
+  });
+
+  test('renders navigation links with correct text and href attributes', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const usersLink = screen.getByRole('link', { name: 'View All Users' });
+    const submissionsLink = screen.getByRole('link', { name: 'View All Submissions' });
+
+    expect(homeLink).toHaveAttribute('href', '/');
+    expect(usersLink).toHaveAttribute('href', '/users');
+    expect(submissionsLink).toHaveAttribute('href', '/submissions');
+  });
+
+  test('matches snapshot', () => {
+    const { asFragment } = render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
